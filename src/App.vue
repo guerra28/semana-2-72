e7da84fca9b68a82d7a9bc5db907be02787893e5
guerra28 <juancm1823@hotmail.com>
2020-12-05 17:01:37 -0500

    esquelto booststrap y seccion noticas en vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    
-    
-    
+    <div class="mr-4 ml-4 mb-5">
+      
+        <seccion-noticias></seccion-noticias>
+        
+    </div>
   </div>
+  
 </template>
 
 <script>
+import SeccionNoticias from "./components/SeccionNoticias.vue";
+
 export default {
+  components: { SeccionNoticias },
   name: "App",
 };
 </script>
 
+
+    SeccionNoticias
