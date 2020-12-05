d8ea164378cd0e7f5dc529eba2aedf48cd7705ee
guerra28 <juancm1823@hotmail.com>
2020-12-05 10:12:33 -0500

    Commit inicial

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['member']
+    }
+</script>
+
+<style scoped>
+
+</style>
