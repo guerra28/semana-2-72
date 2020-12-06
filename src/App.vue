189a26b5cd97c8d7a5d6554c4b760ebbea94083c
guerra28 <juancm1823@hotmail.com>
2020-12-06 10:21:39 -0500

    se agrega datos de miembros

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
 <template>
   <div id="app">
     <div class="mr-4 ml-4 mb-5">
-      
-        <seccion-noticias></seccion-noticias>
-        
+      <seccion-noticias></seccion-noticias>
     </div>
   </div>
-  
 </template>
 
 <script>
 import SeccionNoticias from "./components/SeccionNoticias.vue";
 
 export default {
-  components: { SeccionNoticias },
+
   name: "App",
+  components: { SeccionNoticias },
+  data(){
+    return {
+     
+
+      equipo:[
+
+    { 
+      codigo:1,
+      nombre: "Nombre del miembro de equipo",
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+      rol:"Desarrolador backend",
+      image:"/src/assets/logo.png",
+    },
+    { 
+      codigo:1,
+      nombre: "Nombre del miembro de equipo",
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+      rol:"Desarrolador backend",
+      image:"/src/assets/logo.png",
+    },
+    { 
+      codigo:1,
+      nombre: "Nombre del miembro de equipo",
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+      rol:"Desarrolador backend",
+      image:"/src/assets/logo.png",
+    },
+    { 
+      codigo:1,
+      nombre: "Nombre del miembro de equipo",
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+      rol:"Desarrolador backend",
+      image:"/src/assets/logo.png",
+    },
+    { 
+      codigo:1,
+      nombre: "Nombre del miembro de equipo",
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+      rol:"Desarrolador backend",
+      image:"/src/assets/logo.png",
+    }
+
+      ]
+
+      }
+  }
 };
 </script>
-
-
-   
--- src/components/SeccionNoticias.vue
+++ src/components/SeccionNoticias.vue
@@ -6,7 +6,11 @@
       :key="index"
     >
       <div class="card">
-      <img :src="noticia.urlToImage" class="img-fluid mt-2 ml-2 mr-2" alt="imagen" />
+        <img
+          :src="noticia.urlToImage"
+          class="img-fluid mt-2 ml-2 mr-2"
+          alt="imagen"
+        />
         <div class="card-body">
           <h5 class="card-title">{{ noticia.title }}</h5>
           <h6 class="card-sub-title">
@@ -15,7 +19,9 @@
           <p class="card-text">
             {{ noticia.content }}
           </p>
-          <a :href="noticia.url" class="btn btn-primary" target="_blank">Leer más-></a>
+          <a :href="noticia.url" class="btn btn-primary" target="_blank"
+            >Leer más-></a
+          >
         </div>
       </div>
     </div>
@@ -41,12 +47,9 @@ export default {
 
       .then((response) => {
         this.noticias = response.data.articles.slice(0, 4);
-
-        
       });
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
